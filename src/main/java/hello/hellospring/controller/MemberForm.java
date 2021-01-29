7fcd937461b6d31e8cbabb47ca2f97b6e41462dd
treasureBear94 <qvo7896@gmail.com>
2021-01-29 23:43:02 +0900

    :sparkles: 등록하기

---
+++ src/main/java/hello/hellospring/controller/MemberForm.java
@@ -0,0 +1,20 @@
+package hello.hellospring.controller;
+
+/**
+ * @author yjjung
+ * @version 0.1.0
+ * @since 2021/01/29
+ */
+public class MemberForm {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+}
